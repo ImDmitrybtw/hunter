f68743ad77e25503d08851be3f70af5f706f7165
dmitry <paul.dm23@ya.ru>
2022-04-28 20:39:58 +0300

    hi

--- cmake/projects/hello_foo/hunter.cmake
+++ cmake/projects/hello_foo/hunter.cmake
@@ -7,11 +7,11 @@ hunter_add_version(
         PACKAGE_NAME
         hello_foo
         VERSION
-        "1.0.0"
+        "1.0.3"
         URL
-        "https://github.com/ImDmitrybtw/hello_foo/archive/v1.0.0.tar.gz"
+        "https://github.com/ImDmitrybtw/hello_foo/archive/v1.0.3.tar.gz"
         SHA1
-        b7d963e76fa54a9c47de2fb3d96aa18896f5145c
+        b2b82722065bbf39817784046cc90947c8b1387a
 )
 
 hunter_pick_scheme(DEFAULT url_sha1_cmake)
